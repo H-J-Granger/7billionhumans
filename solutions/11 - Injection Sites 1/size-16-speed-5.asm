58be8fbd5feb4e905363ad7fc1ca5d602a817ce5
Mirai Kuriyama <hermione_granger@foxmail.com>
2023-05-19 22:34:14 +0800

    11: Size 16, Speed, 5

---
+++ solutions/11 - Injection Sites 1/size-16-speed-5.asm
@@ -0,0 +1,28 @@
+-- 7 Billion Humans (2056) --
+-- 11: Injection Sites 1 --
+
+-- Author: Kuriyama Mirai
+-- Size: 16
+-- Speed: 5
+
+pickup s
+step s
+step s
+step s
+step s
+step s
+if n != nothing and
+ s != nothing:
+	step s
+	step s
+	step s
+	step s
+endif
+if n == nothing:
+	step n
+else:
+	step s
+endif
+drop
+
+
