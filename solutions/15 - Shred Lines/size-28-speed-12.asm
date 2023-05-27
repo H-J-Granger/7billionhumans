130d2590610d1504f20666f2b227d068c442f03d
Mirai Kuriyama <hermione_granger@foxmail.com>
2023-05-27 16:19:54 +0800

    15: Size 28, Speed 12

---
+++ solutions/15 - Shred Lines/size-28-speed-12.asm
@@ -0,0 +1,37 @@
+-- 7 Billion Humans (2056) --
+-- 15: Shred Lines --
+
+-- Author: Kuriyama Mirai
+-- Size: 28
+-- Speed: 12
+
+step n
+step n
+step n
+step n
+step n
+pickup n,c
+step s
+step s
+step s
+step s
+step s
+giveto s
+step n
+step n
+step n
+step n
+pickup n,c,s
+step s
+step s
+step s
+step s
+giveto s
+step n
+step n
+pickup n,c
+step s
+step s
+giveto s
+
+
