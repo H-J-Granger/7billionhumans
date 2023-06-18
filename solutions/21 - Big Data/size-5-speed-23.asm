234439aded42c1727c62538c357569fca62159c2
Mirai Kuriyama <hermione_granger@foxmail.com>
2023-06-18 12:46:20 +0800

    21: Size 5, Speed 23

---
+++ solutions/21 - Big Data/size-5-speed-23.asm
@@ -0,0 +1,15 @@
+-- 7 Billion Humans (2056) --
+-- 21: Big Data --
+
+-- Author: Kuriyama Mirai
+-- Size: 5
+-- Speed: ~23
+
+a:
+step s
+takefrom s,se
+if myitem < 50 or
+ myitem == nothing:
+	giveto sw
+	jump a
+endif
