62772d91c0f3e7c5846bdb9392009c28001083b2
Mirai Kuriyama <hermione_granger@foxmail.com>
2023-06-18 13:28:44 +0800

    22: Size 9, Speed ~4.03

---
+++ solutions/22 - Number Royale/size-9-speed-4.asm
@@ -0,0 +1,21 @@
+-- 7 Billion Humans (2056) --
+-- 22: Number Royale --
+
+-- Author: Kuriyama Mirai
+-- Size: 9
+-- Speed: ~4.03 (73 tests)
+-- Success Rate: ~51.60%
+
+step s
+step s
+pickup n
+if myitem < 93:
+	step s
+endif
+a:
+if myitem < e or
+ myitem < w:
+	step s
+endif
+step w
+jump a
