1884c6caa530deb133b9da00af2eb8cbd7365059
Mirai Kuriyama <hermione_granger@foxmail.com>
2023-06-20 23:34:21 +0800

    05: Size 4, Speed ~45 (<25%)

---
+++ solutions/05 - An Important Decision/size-4-speed-45-low.asm
@@ -0,0 +1,15 @@
+-- 7 Billion Humans (2056) --
+-- 5: An Important Decision --
+
+-- Author: Kuriyama Mirai
+-- Size: 4
+-- Speed: ~45 (1 test)
+-- Success Rate: ~6.997% (24/343)
+
+a:
+if e == datacube and
+ w == hole:
+	step e
+endif
+step w,e
+jump a
