6667547ce3b3cccdc145f478526aa6711a34eb72
Mirai Kuriyama <hermione_granger@foxmail.com>
2023-06-21 00:09:17 +0800

    06: Size 7, Speed 6 (<50%)

---
+++ solutions/06 - Little Exterminator 1/size-7-speed-6-low.asm
@@ -0,0 +1,18 @@
+-- 7 Billion Humans (2056) --
+-- 6: Little Exterminator 1 --
+
+-- Author: Kuriyama Mirai
+-- Size: 7
+-- Speed: 6
+-- Success Rate: 25%
+
+a:
+step w,e
+step se
+step sw
+pickup s,se
+step sw
+step se
+jump a
+
+
