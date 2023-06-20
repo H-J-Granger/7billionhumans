b122a5cc4eb4672900daf079114860af566150c4
Mirai Kuriyama <hermione_granger@foxmail.com>
2023-06-20 23:12:01 +0800

    22: Size 4, Speed 1 (<50%)

---
+++ solutions/22 - Number Royale/size-4-speed-1.asm
@@ -0,0 +1,14 @@
+-- 7 Billion Humans (2056) --
+-- 22: Number Royale --
+
+-- Author: hingston (https://github.com/hingston/7-billion-humans-solutions/blob/master/SolutionsLowPercent/Year%2022%20-%20Number%20Royale%20(both).txt)
+-- Size: 4
+-- Speed: 1
+-- Success Rate: ~38.74%
+
+pickup s
+if myitem < 90:
+	a:
+	step s
+	jump a
+endif
